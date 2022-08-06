dd5319891b0a3cdca28609233340ec0927cb7440
NestorSanchez <nesaenz.69@gmail.com>
2022-08-05 19:22:03 -0500

    add state Error for validation formulary

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { MARCAS, YEARS, PLANS } from "../constants";
+import { useCotizador } from "../hook/useCotizador";
 
 export const Formulary = () => {
+  const { handleChangeData, dataForm, error, setError } = useCotizador();
+
+  const handleSubmitData = (e) => {
+    e.preventDefault();
+
+    if (Object.values(dataForm).includes("")) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmitData}>
+        {error && <p>Todos los campos son requeridos mi compa</p>}
         <div className="my-5">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
             Marca
           </label>
           <select
             name="marca"
+            value={dataForm.marca}
             className="w-full p-3 bg-white border border-gray-200"
+            onChange={({ target }) => handleChangeData({ target })}
           >
             <option value="">-- Selecciona Marca --</option>
             {MARCAS.map((marca) => (
-              <option key={marca.id} value={marca.id}>
+              <option key={marca.id} value={marca.name}>
                 {marca.name}
               </option>
             ))}
@@ -26,8 +41,10 @@ export const Formulary = () => {
             Año
           </label>
           <select
-            name="marca"
+            name="year"
+            value={dataForm.year}
             className="w-full p-3 bg-white border border-gray-200"
+            onChange={({ target }) => handleChangeData({ target })}
           >
             <option value="">-- Selecciona Año --</option>
             {YEARS.map((year) => (
@@ -45,7 +62,12 @@ export const Formulary = () => {
             {PLANS.map((plan) => (
               <span key={plan.id}>
                 <label>{plan.name}</label>
-                <input type="radio" name="plan" value={plan.id} />
+                <input
+                  type="radio"
+                  name="plan"
+                  value={plan.id}
+                  onChange={({ target }) => handleChangeData({ target })}
+                />
               </span>
             ))}
           </div>
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { createContext } from "react";
 
 export const CotizadorContext = createContext();
 
 export const CotizadorProvider = ({ children }) => {
+  const [error, setError] = useState(false);
+  const [dataForm, setDataForm] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+
+  const handleChangeData = ({ target }) => {
+    setDataForm({
+      ...dataForm,
+      [target.name]: target.value,
+    });
+  };
   return (
-    <CotizadorContext.Provider value={{}}>{children}</CotizadorContext.Provider>
+    <CotizadorContext.Provider
+      value={{ handleChangeData, dataForm, error, setError }}
+    >
+      {children}
+    </CotizadorContext.Provider>
   );
 };
